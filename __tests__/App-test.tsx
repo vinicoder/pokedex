18ea25f1e998c4f1a87591ea492cbad0e369e22f
Vinicius de Moraes <vinicoder@gmail.com>
2020-06-09 13:21:52 -0300

    init: tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,13 +1,11 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import App from '../src/App';
+
+jest.mock('react-native-gesture-handler', () => {});
+jest.mock('@react-native-community/async-storage', () => {});
+jest.mock('@react-navigation/stack', () => {});
 
 it('renders correctly', () => {
   renderer.create(<App />);
